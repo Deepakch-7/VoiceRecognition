f2b47733843a67ffe262c2f995e7aa7fed4789c4
Deepak Chelluboina <chd25090@gmail.com>
2026-06-06 15:35:22 +0530

    Remove tensorflow dependency, use tflite directly

--- model/keypoint_classifier/keypoint_classifier.py
+++ model/keypoint_classifier/keypoint_classifier.py
@@ -1,36 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import numpy as np
-import tensorflow as tf
+import mediapipe as mp
 
-
-class KeyPointClassifier(object):
-    def __init__(
-        self,
-        model_path='model/keypoint_classifier/keypoint_classifier.tflite',
-        num_threads=1,
-    ):
-        self.interpreter = tf.lite.Interpreter(model_path=model_path,
-                                               num_threads=num_threads)
-
+class KeyPointClassifier:
+    def __init__(self, model_path='model/keypoint_classifier/keypoint_classifier.tflite'):
+        import importlib
+        try:
+            import tflite_runtime.interpreter as tflite
+            self.interpreter = tflite.Interpreter(model_path=model_path)
+        except ImportError:
+            import tensorflow as tf
+            self.interpreter = tf.lite.Interpreter(model_path=model_path)
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
-    def __call__(
-        self,
-        landmark_list,
-    ):
-        input_details_tensor_index = self.input_details[0]['index']
-        self.interpreter.set_tensor(
-            input_details_tensor_index,
-            np.array([landmark_list], dtype=np.float32))
+    def __call__(self, landmark_list):
+        input_data = np.array([landmark_list], dtype=np.float32)
+        self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
         self.interpreter.invoke()
-
-        output_details_tensor_index = self.output_details[0]['index']
-
-        result = self.interpreter.get_tensor(output_details_tensor_index)
-
-        result_index = np.argmax(np.squeeze(result))
-
-        return result_index
+        result = self.interpreter.get_tensor(self.output_details[0]['index'])
+        return np.argmax(np.squeeze(result))
--- model/point_history_classifier/point_history_classifier.py
+++ model/point_history_classifier/point_history_classifier.py
@@ -1,44 +1,20 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 import numpy as np
-import tensorflow as tf
 
-
-class PointHistoryClassifier(object):
-    def __init__(
-        self,
-        model_path='model/point_history_classifier/point_history_classifier.tflite',
-        score_th=0.5,
-        invalid_value=0,
-        num_threads=1,
-    ):
-        self.interpreter = tf.lite.Interpreter(model_path=model_path,
-                                               num_threads=num_threads)
-
+class PointHistoryClassifier:
+    def __init__(self, model_path='model/point_history_classifier/point_history_classifier.tflite'):
+        try:
+            import tflite_runtime.interpreter as tflite
+            self.interpreter = tflite.Interpreter(model_path=model_path)
+        except ImportError:
+            import tensorflow as tf
+            self.interpreter = tf.lite.Interpreter(model_path=model_path)
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
-        self.score_th = score_th
-        self.invalid_value = invalid_value
-
-    def __call__(
-        self,
-        point_history,
-    ):
-        input_details_tensor_index = self.input_details[0]['index']
-        self.interpreter.set_tensor(
-            input_details_tensor_index,
-            np.array([point_history], dtype=np.float32))
+    def __call__(self, point_history_list):
+        input_data = np.array([point_history_list], dtype=np.float32)
+        self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
         self.interpreter.invoke()
-
-        output_details_tensor_index = self.output_details[0]['index']
-
-        result = self.interpreter.get_tensor(output_details_tensor_index)
-
-        result_index = np.argmax(np.squeeze(result))
-
-        if np.squeeze(result)[result_index] < self.score_th:
-            result_index = self.invalid_value
-
-        return result_index
+        result = self.interpreter.get_tensor(self.output_details[0]['index'])
+        return np.argmax(np.squeeze(result))
